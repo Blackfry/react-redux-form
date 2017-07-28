92acc988bd3ac93c86bc6169f657d792243d5b02
Mark Dickie <mdickie1@gmail.com>
2017-07-28 23:00:28 +1000

    Update deep-compare-children.js
    change import shallowCompare

--- src/utils/deep-compare-children.js
+++ src/utils/deep-compare-children.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import shallowCompare from 'shallow-compare';
+import shallowCompare from 'react-addons-shallow-compare';
 import shallowEqual from './shallow-equal';
 
 export function compareChildren(props, nextProps) {
